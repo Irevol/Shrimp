6afbc05fe3796667c13ad8eb61a02c0332b0f56a
Irevol <141366212+Irevol@users.noreply.github.com>
2026-01-23 13:11:45 -0700

    Bullet anim + fix

--- Scripts/Rewards/Win.gd
+++ Scripts/Rewards/Win.gd
@@ -1,1 +1,11 @@
 extends Reward
+
+
+func before_pickup():
+	sprite.sprite_frames = load("res://Animations/rice.tres")
+	sprite.play("default")
+	set_description("Shrimp Fried Rice","""You've beat the game!\nEnjoy a delicious treat""")
+	
+
+func on_pickup():
+	game_control.on_win()
